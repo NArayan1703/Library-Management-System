0e5f8cf573ae363785f6a40a3cd1904259057c23
Narayan Adhikari <130231554+NArayan1703@users.noreply.github.com>
2024-01-26 09:29:31 +0545

    Update Book.java

--- src/main/java/com/example/projectfx/Book.java
+++ src/main/java/com/example/projectfx/Book.java
@@ -1,2 +1,43 @@
-package com.example.projectfx;public class Book {
+public class Book {
+    private int id;
+    private String title;
+    private String author;
+    private boolean available;
+
+    public Book(int id, String title, String author, boolean available) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.available = available;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", available=" + available +
+                '}';
+    }
 }
